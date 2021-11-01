55b93fa7a6371d30a8ec9083ecaad039ca259adb
Luke Emrys Walker <81542899+cherry-xd@users.noreply.github.com>
2021-11-01 02:30:10 +0000

    added try and except so it doesnt crash

--- input.py
+++ input.py
@@ -3,6 +3,10 @@ from abc import abstractmethod
 class Input:
     @abstractmethod
     def read(input):
-        dict = {"row" : input[3], "col" : input[1], "row" : input[0]}
-        print(dict)
-        return dict
+        while True:
+            try: 
+                dict = {"row" : input[3], "col" : input[1], "row" : input[0]}
+                print(dict)
+                return dict
+            except:
+                print("Invalid input")
