fa4749e76833f2053c8ec45157d1ddd099bca47b
Luke Emrys Walker <81542899+cherry-xd@users.noreply.github.com>
2021-11-01 02:31:32 +0000

    everything here shouldn't have been here, made it a class instead

--- settings.py
+++ settings.py
@@ -1,36 +1,9 @@
-from cells import Grid
-import main
+class Settings:
+    def __init__(self):
+        self.diffs = {"easy" : 4.1, "medium" : 3.75, "hard" : 3.15, "hidden" : 2.5}
+        self.gridSize = 7
+        self.difficulty = "medium"
 
-def settings():
-
-    prompt = input("'G' = Grid Size, 'D' = Difficulty, 'B' = Back: ")
-    
-    if prompt == "G":
-        size = input("Type desired grid size (default 5): ")
-        try:
-            if int(size) >= 2 and int(size) <= 8:
-                Grid.setSize(int(size))
-                print("Set grid size to {}".format(size))
-                settings()
-        except:
-            print("Invalid grid size")
-            settings()
-            
-    if prompt == "D":
-        diff = input("'Easy', 'Medium', 'Hard' (default Medium): ")
-        if diff == "Easy" or "Medium" or "Hard":
-            print("Set difficulty to {}".format(diff))
-            diffs = {"Easy" : 4.5, "Medium" : 4, "Hard" : 3.5}
-            diffScale = diffs.get(diff)
-            Grid.calcDiff(diffScale, diff)
-            settings()
-        else:
-            print("Unrecognised difficulty")
-            settings()
-            
-    if prompt == "B":
-        main.main()
-
-    else:
-        print("Unrecognised command")
-        settings()
+    def calcDiff(self):
+        self.diffScale = self.diffs.get(self.difficulty)
+        self.minecount = round((self.gridSize ** 2) / self.diffScale)
