1fa873ea6274b7acec4accf3980a017215ff464c
cherry-xd <lukeww101@gmail.com>
2021-11-02 04:19:50 +0000

    fixed difficulty, added basic gameplay

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import cells ; from cells import Grid
 from input import Input
-from settings import Settings
 
+## this is done this way to avoid a circular import, it works so i dont think i need any other solution right now
 settings = cells.getSettings()
 
 def main():
-    prompt = input("Type 'Play', 'Grid size', or 'Difficulty': ") 
+    prompt = input("\nType 'Play', 'Grid size', or 'Difficulty': ") 
     if prompt.lower() in {"play", "p"}:
         drawGame()
-        return
-    
-    if prompt.lower() in {"grid size", "g", "gridsize", "s", "size"}:
+
+    elif prompt.lower() in {"grid size", "g", "gridsize", "s", "size"}:
         while True:
             size = input("Type desired grid size (current {}): ".format(settings.gridSize))
             try:
@@ -23,7 +22,7 @@ def main():
             except:
                 print("Invalid grid size")
             
-    if prompt.lower() in {"difficulty", "d", "diff"}:
+    elif prompt.lower() in {"difficulty", "d", "diff"}:
         while True:
             difficulty = input("'Easy', 'Medium', 'Hard' (current {}): ".format(settings.difficulty.capitalize()))
             if difficulty.lower() in settings.diffs.keys():
@@ -38,14 +37,41 @@ def main():
         main()
 
 def drawGame():
-    print("\nStarting game!\nGrid size: {}\nDifficulty: {}".format(settings.gridSize, settings.difficulty.capitalize()))
-    grid = Grid()
-    grid.draw()
     settings.calcDiff()
-
-    prompt = input("Enter desired cell ({} mines left): ".format(settings.minecount))
-    print(prompt)
-    dict = Input.read(prompt)
+    grid = Grid()
+    minecount = 0
+    ## i thought about making this check to see if a mine is already present and then skip that cell if true but i like the randomness of this
+    for i in range(settings.mines):
+        x, y = grid.randomCell()
+        cell = grid.grid[x][y]
+        if not cell.isMine():
+            cell.mine = True
+            minecount += 1
+    print("\nStarting game!\nGrid size: {}\nDifficulty: {}\nMines: {}\n(format for input: 'row' 'col' 'O = open, F = flag')\nEG. 'ado' opens row a, column d".format(settings.gridSize, settings.difficulty.capitalize(), minecount))
+    grid.draw()
+            
+    while True:
+        prompt = input("Enter desired cell: ")
+        dict = Input.read(prompt)
+        ## i do not need validation for < 0 as there is no letter which corresponds to 0 or < 0
+        if dict["row"] > settings.gridSize or dict["col"] > settings.gridSize:
+            print("Cell entered does not exist")
+        else:
+            cell = grid.grid[dict["row"]][dict["col"]]
+            print(cell)
+            if dict["action"].lower() == "o":
+                cell.openCell()
+                if cell.isMine():
+                    grid.draw()
+                    input("\nGAME OVER!!! type anything to return to the menu: ")
+                    main()
+                    return
+                else: 
+                    grid.draw()
+            if dict["action"].lower() == "f":
+                cell.setFlag()
+                grid.draw()
+            
 
 def getSettings():
     return settings
