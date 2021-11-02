85f29ed74bb6142fc637aa076b37aa8036b03d32
cherry-xd <lukeww101@gmail.com>
2021-11-02 04:20:08 +0000

    tweaked values, optimised code

--- settings.py
+++ settings.py
@@ -1,9 +1,10 @@
 class Settings:
     def __init__(self):
-        self.diffs = {"easy" : 4.1, "medium" : 3.75, "hard" : 3.15, "hidden" : 2.5}
+        ## i did this for easy expandability
+        self.diffs = {"easy" : 4, "medium" : 3.65, "hard" : 3, "hidden" : 2.15}
         self.gridSize = 7
         self.difficulty = "medium"
 
     def calcDiff(self):
-        self.diffScale = self.diffs.get(self.difficulty)
-        self.minecount = round((self.gridSize ** 2) / self.diffScale)
+        self.diffScale = self.diffs[self.difficulty]
+        self.mines = round((self.gridSize ** 2) / self.diffScale)
