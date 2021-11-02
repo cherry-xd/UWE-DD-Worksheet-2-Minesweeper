010f48933c676c63f30fe86365780601bff17ab3
Luke Emrys Walker <81542899+cherry-xd@users.noreply.github.com>
2021-11-02 17:48:18 +0000

    comment

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 class Settings:
     def __init__(self):
-        ## i did this for easy expandability
+        ## i did this for easy expandability, i'm also considering using json files to store these settings more permanently
         self.diffs = {"easy" : 4, "medium" : 3.65, "hard" : 3, "hidden" : 2.15}
         self.gridSize = 7
         self.difficulty = "medium"
